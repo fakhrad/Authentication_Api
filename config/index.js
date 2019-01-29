329681d0e386a7c35319c71422bef1cec5aebbe0
saeedPadyab <padyabsaeed@gmail.com>
2019-01-29 03:01:24 +0300

    added function instead of hoc

--- config/index.js
+++ config/index.js
@@ -1,12 +1,12 @@
 import Config from "react-native-config";
 const authApis = {
-  BASE_URL: Config.authentiation.BASE_URL || "http://13.69.148.176:8075",
-  LOGIN_URL: Config.authentiation.LOGIN_URL || "/shopper/api/v1/login",
-  SIGNUP_URL: Config.authentiation.SIGNUP_URL || "/shopper/api/v1/signup",
-  VERIFY_CODE: Config.authentiation.VERIFY_CODE || "/shopper/api/v1/verifycode",
-  CHANGE_CITY: Config.authentiation.CHANGE_CITY || "/shopper/api/v1/changecity",
-  GET_CITIES: Config.authentiation.GET_CITIES || "/cities/api/v1/getall",
+  BASE_URL: Config.BASE_URL || "http://13.69.148.176:8075",
+  LOGIN_URL: Config.LOGIN_URL || "/shopper/api/v1/login",
+  SIGNUP_URL: Config.SIGNUP_URL || "/shopper/api/v1/signup",
+  VERIFY_CODE: Config.VERIFY_CODE || "/shopper/api/v1/verifycode",
+  CHANGE_CITY: Config.CHANGE_CITY || "/shopper/api/v1/changecity",
+  GET_CITIES: Config.GET_CITIES || "/cities/api/v1/getall",
   GET_USER_INFO:
-    Config.authentiation.GET_USER_INFO || "/shopper/api/v1/getshopperinfo"
+    Config.GET_USER_INFO || "/shopper/api/v1/getshopperinfo"
 };
 export { authApis };
--- index.js
+++ index.js
@@ -1,8 +1,212 @@
-import LoginApi from "./hocs/Login";
-import SignupApi from "./hocs/Signup";
-import VerifyCodeApi from "./hocs/VerifyCode";
-import ChangeCityApi from "./hocs/ChangeCity";
-import GetCitiesApi from "./hocs/GetCities";
-import GetUserInfoApi from "./hocs/GetUserInfo";
+import { Alert } from "react-native";
+import { authApis } from "./config";
+import { languageManager } from "@app-sdk/services";
 
-export { LoginApi, SignupApi, VerifyCodeApi, ChangeCityApi ,GetCitiesApi , GetUserInfoApi};
+
+const logIn = async phoneNumber => {
+    try {
+        const url = authApis.BASE_URL + authApis.LOGIN_URL;
+        var rawResponse = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                phoneNumber: numb
+            })
+        });
+        const status = rawResponse.status;
+        if (status == 200 || status == 201) {
+            // exisiting user . created
+            return rawResponse;
+        } else if (status == 500) {
+            // internal skserver error
+            const error = translate.t("LOGIN_ERROR_500");
+            alertError(error);
+            return undefined;
+        } else if (status == 400) {
+            //  phone number is required
+            const error = translate.t("LOGIN_ERROR_400");
+            alertError(error);
+            return undefined;
+        }
+    } catch (error) {
+        alertError(error.message);
+        return undefined;
+    }
+};
+const signUp = async phoneNumber => {
+    try {
+        const url = authApis.BASE_URL + authApis.SIGNUP_URL;
+        var rawResponse = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                phoneNumber: phoneNumber
+            })
+        });
+        const status = rawResponse.status;
+        if (status == 200 || status == 201) {
+            // exisiting user . created
+            return rawResponse;
+        } else if (status == 500) {
+            // internal server error
+            const error = translate.t("SIGNUP_ERROR_500");
+            alertError(error);
+            return undefined;
+        } else if (status == 400) {
+            //  phone number is required
+            const error = translate.t("LOGIN_ERROR_400");
+            alertError(error);
+            return undefined;
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+// const getCities = async () => {
+//   var cities = await appStorage.getItem("cities");
+//   if (cities != undefined && cities != null && cities.length > 0) {
+//     return JSON.parse(cities);
+//   }
+//   try {
+//     const url = authApis.BASE_URL + authApis.GET_CITIES;
+//     var rawResponse = await fetch(url);
+//     const status = rawResponse.status;
+//     if (status == 200) {
+//       var cities = await rawResponse.json();
+//       appStorage.setItem("cities", JSON.stringify(cities));
+//       return cities;
+//     } else if (status == 500) {
+//       // internal server error
+//       const error = translate.t("GET_CITIES_ERROR_500");
+//       alertError(error);
+//       return undefined;
+//     }
+//   } catch (error) {
+//     console.log(error.message);
+//     return undefined;
+//   }
+// };
+
+// const getShopperInfo = async (token, phoneNumber) => {
+//   try {
+//     const url =
+//       authApis.BASE_URL +
+//       authApis.GET_SHOPER_INFO +
+//       "?phoneNumber=" +
+//       phoneNumber;
+//     var rawResponse = await fetch(url, {
+//       method: "GET",
+//       headers: {
+//         authorization: "Bearer " + token,
+//         "Content-Type": "application/json"
+//       }
+//     });
+//     const status = rawResponse.status;
+//     if (status == 200) {
+//       rawResponse.json().then(userInfo => {
+//         store.dispatch({ type: types.SAVE_USER_INFO, value: userInfo });
+//       });
+//     } else if (status == 500) {
+//       const error = translate.t("GET_SHOPPER_INFO_ERROR_500");
+//       alertError(error);
+//     } else if (status == 400) {
+//       // invalid phone number
+//       removeUserInfo();
+//       initializing();
+//     } else if (status == 401) {
+//       // UnAuthorized;
+//       removeUserInfo();
+//       initializing();
+//     } else if (status == 404) {
+//       // not found
+//       removeUserInfo();
+//       initializing();
+//     }
+//   } catch (error) {
+//     alertError(error.message);
+//   }
+// };
+// const verifyCode = async (phoneNumber, code) => {
+//   try {
+//     const url = authApis.BASE_URL + authApis.VERIFY_CODE;
+//     var rawResponse = await fetch(url, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json"
+//       },
+//       body: JSON.stringify({
+//         phoneNumber: phoneNumber,
+//         code: code
+//       })
+//     });
+//     const status = rawResponse.status;
+//     if (status == 200) {
+//       return rawResponse;
+//     } else if (status == 500) {
+//       // server error
+//       const error = translate.t("VERIFY_CODE_ERROR_500");
+//       alertError(error);
+//     } else if (status == 400) {
+//       // invalid code = bad request
+//       const error = translate.t("VERIFY_CODE_ERROR_400");
+//       alertError(error);
+//     } else if (status == 404) {
+//       // invalid phone number- not found
+//       const error = translate.t("VERIFY_CODE_ERROR_404");
+//       alertError(error);
+//     }
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// };
+
+// const changeCity = async cityCode => {
+//   try {
+//     const url = authApis.BASE_URL + authApis.CHANGE_CITY;
+//     const userInfo = store.getState().userReducer.userInfo;
+//     const token = userInfo.token;
+//     var rawResponse = await fetch(url, {
+//       method: "PUT",
+//       headers: {
+//         authorization: "Bearer " + token,
+//         "Content-Type": "application/json"
+//       },
+//       body: JSON.stringify({
+//         id: userInfo._id,
+//         citycode: cityCode
+//       })
+//     });
+//     const status = rawResponse.status;
+//     if (status == 200) {
+//       return rawResponse;
+//     } else if (status == 500) {
+//       // server error
+//       const error = translate.t("CHANGE_CITY_ERROR_500");
+//       alertError(error);
+//       return undefined;
+//     } else if (status == 400) {
+//       // invalid city code
+//       const error = translate.t("CHANGE_CITY_ERROR_400");
+//       alertError(error);
+//       return undefined;
+//     } else if (status == 404) {
+//       // can not found city
+//       const error = translate.t("CHANGE_CITY_ERROR_404");
+//       alertError(error);
+//       return undefined;
+//     }
+//   } catch (error) {
+//     console.log(error.message);
+//     return undefined;
+//   }
+// };
+
+alertError = error => {
+  Alert.alert(error);
+};
+export { logIn, verifyCode, signUp, changeCity, getCities, getShopperInfo };
