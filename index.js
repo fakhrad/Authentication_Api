b8ca93f780222c3c39af860a539b00f4b4b9e485
saeedPadyab <padyabsaeed@gmail.com>
2019-02-03 21:41:49 +0300

    change city and getCities added

--- index.js
+++ index.js
@@ -59,30 +59,39 @@ const verifyCode = async ({ phoneNumber, code }) => {
         console.log(error.message);
     }
 };
-// const getCities = async () => {
-//   var cities = await appStorage.getItem("cities");
-//   if (cities != undefined && cities != null && cities.length > 0) {
-//     return JSON.parse(cities);
-//   }
-//   try {
-//     const url = authApis.BASE_URL + authApis.GET_CITIES;
-//     var rawResponse = await fetch(url);
-//     const status = rawResponse.status;
-//     if (status == 200) {
-//       var cities = await rawResponse.json();
-//       appStorage.setItem("cities", JSON.stringify(cities));
-//       return cities;
-//     } else if (status == 500) {
-//       // internal server error
-//       const error = translate.t("GET_CITIES_ERROR_500");
-//       alertError(error);
-//       return undefined;
-//     }
-//   } catch (error) {
-//     console.log(error.message);
-//     return undefined;
-//   }
-// };
+
+const changeCity = async ({ cityCode }) => {
+    try {
+        const url = authApis.BASE_URL + authApis.CHANGE_CITY;
+        const { token, _id } = authManager.instance.currentUser;
+        var rawResponse = await fetch(url, {
+            method: "PUT",
+            headers: {
+                authorization: "Bearer " + token,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                id: _id,
+                citycode: cityCode
+            })
+        });
+        return rawResponse;
+    } catch (error) {
+        console.log(error.message);
+        return undefined;
+    }
+};
+
+const getCities = async () => {
+    try {
+        const url = authApis.BASE_URL + authApis.GET_CITIES;
+        var rawResponse = await fetch(url);
+        return rawResponse;
+    } catch (error) {
+        console.log(error.message);
+        return undefined;
+    }
+};
 
 // const getShopperInfo = async (token, phoneNumber) => {
 //   try {
@@ -124,48 +133,7 @@ const verifyCode = async ({ phoneNumber, code }) => {
 //   }
 // };
 
-// const changeCity = async cityCode => {
-//   try {
-//     const url = authApis.BASE_URL + authApis.CHANGE_CITY;
-//     const userInfo = store.getState().userReducer.userInfo;
-//     const token = userInfo.token;
-//     var rawResponse = await fetch(url, {
-//       method: "PUT",
-//       headers: {
-//         authorization: "Bearer " + token,
-//         "Content-Type": "application/json"
-//       },
-//       body: JSON.stringify({
-//         id: userInfo._id,
-//         citycode: cityCode
-//       })
-//     });
-//     const status = rawResponse.status;
-//     if (status == 200) {
-//       return rawResponse;
-//     } else if (status == 500) {
-//       // server error
-//       const error = translate.t("CHANGE_CITY_ERROR_500");
-//       alertError(error);
-//       return undefined;
-//     } else if (status == 400) {
-//       // invalid city code
-//       const error = translate.t("CHANGE_CITY_ERROR_400");
-//       alertError(error);
-//       return undefined;
-//     } else if (status == 404) {
-//       // can not found city
-//       const error = translate.t("CHANGE_CITY_ERROR_404");
-//       alertError(error);
-//       return undefined;
-//     }
-//   } catch (error) {
-//     console.log(error.message);
-//     return undefined;
-//   }
-// };
-
 alertError = error => {
     Alert.alert(error);
 };
-export { logIn, signUp, verifyCode };
+export { logIn, signUp, verifyCode, changeCity, getCities };
